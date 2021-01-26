8e2c1dfffa589af7dec9af6594e4badcd6a4b9af
Abdulrahman Semrie <hsamireh@gmail.com>
2021-01-26 14:34:15 +0300

    Add script for MOSES evaluating models

---
+++ notebooks/moses_models_eval.py
@@ -0,0 +1,83 @@
+__author__ = 'Abdulrahman Semrie<hsamireh@gmail.com>'
+
+from mozi_cross_val.main.model_evaluator import ModelEvaluator
+from mozi_cross_val.models.objmodel import MosesModel
+import argparse
+import pandas as pd
+import numpy as np
+import re
+import os
+
+output_regex = re.compile(r"(-?\d+) (.+) \[(.+)\]")
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="A script to get scores of MOSES Models")
+    parser.add_argument("--input", type=str, default='',
+                        help="Path to input file to moses")
+    parser.add_argument("--combo", type=str, default='',
+                        help="Path to file that contains combo programs")
+    parser.add_argument("--target", type=str, default='posOutcome',
+                        help="Name of the target column in the input file")
+    parser.add_argument("--output", type=str,
+                        help="Path to save the output file to")
+    return parser.parse_args()
+
+
+def format_combo(combo_file, complexity=False):
+    """
+    Build model objects from the moses output
+    :param combo_file: The path to the raw combo output
+    :return:
+    """
+
+    models = []
+    with open(combo_file, "r") as fp:
+        for line in fp:
+            match = output_regex.match(line.strip())
+            if match is not None:
+                model = match.group(2).strip()
+                if model == "true" or model == "false":
+                    continue
+                if complexity:
+                    complexity = match.group(3).split(",")[2].split("=")[1]
+                    models.append(MosesModel(model, complexity))
+                else:
+                    models.append(MosesModel(model, None))
+
+    return models
+
+def evaluate_models():
+    print("Starting..")
+    args = parse_args()
+    target_col = args.target
+    input_file = args.input
+    output_file = args.output
+    combo_file = args.combo
+
+    if not os.path.exists(input_file):
+        print("Input file %s not found. Please provide the correct path" % input_file)
+        return
+    if not os.path.exists(combo_file):
+        print("Combo file %s not found. Please provide the correct path" % combo_file)
+        return
+    print("Parsing Models...")
+    models = format_combo(combo_file)
+    model_eval = ModelEvaluator(target_col)
+    print("Evaluating Models...")
+    matrix = model_eval.run_eval(models, input_file)
+    scores = model_eval.score_models(matrix, input_file)
+
+    for model, score in zip(models, scores):
+        model.test_score = score
+
+    res_dict = {"model":[], "recall": [], "precision": []}
+    for model in models:
+        res_dict["model"].append(model)
+        res_dict["recall"].append(model.test_score.recall)
+        res_dict["precision"].append(model.test_score.precision)
+        res_dict["accuracy"].append(model.test_score.accuracy)
+
+    output_csv = pd.DataFrame(res_dict)
+    output_csv.to_csv(output_file, index=False)
+
+    print("Done!")
